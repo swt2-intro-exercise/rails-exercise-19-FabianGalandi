e86afbc7d318463d41f6f7fc9e16f27e87dbfdb6
Fabian Galandi <fabian.galandi@student.hpi.de>
2019-11-07 23:42:04 +0100

    Failing test for #14

--- spec/features/paper/paper_index_spec.rb
+++ spec/features/paper/paper_index_spec.rb
@@ -1,9 +1,39 @@
 require 'rails_helper'
 
-describe "New author page", type: :feature do
- 
- it "should render withour error" do
-   visit new_author_path
- end
- 
+describe "Paper index page", type: :feature do
+
+  before(:each) do
+    @test1 = Paper.create(:title => "How to Create a General AI",
+                  :venue => "testVenue",
+                  :year => 1950,
+                  :authors => [])
+
+    @test2 = Paper.create(:title => "How to defeat a General AI Killer Robot",
+                  :venue => "testVenu",
+                  :year => 1968,
+                  :authors => [])
+  end
+
+  it "should render withour error" do
+    visit papers_path
+  end
+
+  it "should show allow filtering papers by year" do
+    visit papers_path
+
+    expect(page).to have_text(@test1.title)
+    expect(page).to have_text(@test2.title)
+
+    visit papers_path(:year => 1950)
+
+    expect(page).to have_text(@test1.title)
+    expect(page).to_not have_text(@test2.title)
+
+    visit papers_path(:year => 1968)
+
+    expect(page).to_not have_text(@test1.title)
+    expect(page).to have_text(@test2.title)
+  end
+
+end 
 end
