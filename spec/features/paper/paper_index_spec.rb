48cafb20326a6d4e17e05db8013049cfb75b1fba
Fabian Galandi <fabian.galandi@student.hpi.de>
2019-11-07 22:15:54 +0100

    Failing test for #9

---
+++ spec/features/paper/paper_index_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe "New author page", type: :feature do
+ 
+ it "should render withour error" do
+   visit new_author_path
+ end
+ 
+end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
-describe "New paper page", type: :feature do
+title = "Test"
+venue = "testVenue"
+year = 2019
 
-  it "should render withour error" do
-    visit new_paper_path
+describe Paper, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have title, venue, and year" do
+    paper = Paper.new()
+    paper.title = title
+    paper.venue = venue 
+    paper.title = title
+    expect(paper.title).to eq(title)
+    expect(paper.venue).to eq(venue)
+    expect(paper.year).to eq(year)
   end
 
-end 
+  it "should not validate without title" do
+    paper = Paper.new( :venue => venue, :year => year)
+    expect(paper).to_not be_valid
+  end
+
+  it "should only validate with numerical year" do
+    paper = Paper.new( :title => title, :venue => venue, :year => title)
+    expect(paper).to_not be_valid
+  end
+end
