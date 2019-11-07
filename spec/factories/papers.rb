6f2c2e20a828c426949a5b801b3f5e92c64caab7
Fabian Galandi <fabian.galandi@student.hpi.de>
2019-11-07 23:12:16 +0100

    resolved #12

--- spec/factories/papers.rb
+++ spec/factories/papers.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :paper do
-    title { "AI writes better AI then Humans" }
+    title { "AI writes better AI than Humans" }
     venue { "AI Town" }
     year { 2019 }
+    authors {build_list :author, 1}
   end
 end
